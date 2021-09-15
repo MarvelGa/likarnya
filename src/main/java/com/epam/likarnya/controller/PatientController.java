486c1fb7a4ffbc8f5140c2ca6bf11be3c90949e4
dxdNvqKP <7VDO8j7kEg>
2021-09-15 11:35:38 +0300

    added sorting for patient page

--- src/main/java/com/epam/likarnya/controller/PatientController.java
+++ src/main/java/com/epam/likarnya/controller/PatientController.java
@@ -2,6 +2,7 @@ package com.epam.likarnya.controller;
 
 import com.epam.likarnya.dto.LoginRequestDto;
 import com.epam.likarnya.dto.PatientDto;
+import com.epam.likarnya.dto.UserDTO;
 import com.epam.likarnya.model.Patient;
 import com.epam.likarnya.repository.PatientRepository;
 import com.epam.likarnya.service.PatientService;
@@ -10,6 +11,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
@@ -18,11 +20,14 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Data
@@ -101,8 +106,51 @@ public class PatientController {
 //    }
 
     @GetMapping(value = "/admin/patients")
-    public String listPatients(@PageableDefault(size = 8) Pageable pageable, Model model) {
+    public String listPatients(@PageableDefault(size = 8) Pageable pageable, @RequestParam(value = "sorting", required = false) String sort, Model model) {
+        int size = pageable.getPageSize();
         Page<Patient> page = patientService.getPatientsWithOutMedicCard(pageable);
+        if (sort != null && page.getTotalElements() != 0) {
+            if (!sort.isEmpty()) {
+                if (sort.equals("ASC-NAME")) {
+                    page = new PageImpl<>(page.stream()
+                            .filter(c -> c != null)
+                            .sorted(Comparator.comparing(Patient::getFirstName))
+                            .collect(Collectors.toList()), pageable, size);
+                }
+                if (sort.equals("DESC-NAME")) {
+                    page = new PageImpl<>(page.stream()
+                            .filter(c -> c != null)
+                            .sorted(Comparator.comparing(Patient::getFirstName).reversed())
+                            .collect(Collectors.toList()), pageable, size);
+                }
+                if (sort.equals("ASC")) {
+                    page = new PageImpl<>(page.stream()
+                            .filter(c -> c != null)
+                            .sorted(Comparator.comparing(Patient::getLastName))
+                            .collect(Collectors.toList()), pageable, size);
+                }
+                if (sort.equals("DESC")) {
+                    page = new PageImpl<>(page.stream()
+                            .filter(c -> c != null)
+                            .sorted(Comparator.comparing(Patient::getLastName).reversed())
+                            .collect(Collectors.toList()), pageable, size);
+                }
+                if (sort.equals("DECREASE")) {
+                    page = new PageImpl<>(page.stream()
+                            .filter(c -> c != null)
+                            .sorted(Comparator.comparing(Patient::getDateOfBirth).reversed())
+                            .collect(Collectors.toList()), pageable, size);
+                }
+                if (sort.equals("INCREASE")) {
+                    page = new PageImpl<>(page.stream()
+                            .filter(c -> c != null)
+                            .sorted(Comparator.comparing(Patient::getDateOfBirth))
+                            .collect(Collectors.toList()), pageable, size);
+                }
+                model.addAttribute("sort", sort);
+            }
+        }
+
         Long totalElements = page.getTotalElements();
         model.addAttribute("page", page);
         model.addAttribute("totalElements", totalElements);
